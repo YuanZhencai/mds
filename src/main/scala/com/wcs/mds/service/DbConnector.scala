647bbd2e5ed2d66c6b6eb210ee998e6adea08d7f
laogao <gaoyuxiang@wcs-global.com>
2014-01-23 01:44:48 +0800

    upgrading to slick 2.0, with DbConnector ready

--- src/main/scala/com/wcs/mds/service/DbConnector.scala
+++ src/main/scala/com/wcs/mds/service/DbConnector.scala
@@ -5,36 +5,21 @@ import akka.actor.ActorLogging
 import com.wcs.mds.model._
 import com.wcs.mds.model.SyncLog
 import slick.driver.PostgresDriver.simple._
-import scala.slick.session.Database
-import Database.threadLocalSession
-import scala.slick.lifted.Query
+import scala.slick.jdbc.JdbcBackend.Database.dynamicSession
 
 class DbConnector extends Actor with ActorLogging {
 
   val conf = context.system.settings.config
   val db = Database.forURL(url = conf.getString("jdbc.url"), driver = conf.getString("jdbc.driver"))
 
-  override def preStart = {
-    super.preStart()
-    db.withSession {
-      import com.wcs.mds.model._
-      val ddl = TabZhrMds001s.ddl ++ TabZhrMds002s.ddl ++ TabZhrMds003s.ddl ++ SyncLogs.ddl
-      ddl.dropStatements foreach { sql =>
-        log.info(sql)
-      }
-      ddl.createStatements foreach { sql =>
-        log.info(sql)
-      }
-    }
-  }
-
   def receive = {
     case msg @ TabZhrMds001(_, _, pernr, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _) =>
       log.info(s"Received $msg.")
       try {
-        db.withTransaction {
-          Query(TabZhrMds001s).filter(_.pernr === pernr).delete
-          TabZhrMds001s.insert(TabZhrMds001.unapply(msg).get)
+        db.withDynTransaction {
+          val tabZhrMds001s = TableQuery[TabZhrMds001s]
+          tabZhrMds001s.filter(_.pernr === pernr).delete
+          tabZhrMds001s.insert(msg)
         }
         log.info(s"Saved $msg.")
       } catch {
@@ -44,9 +29,10 @@ class DbConnector extends Actor with ActorLogging {
     case msg @ TabZhrMds002(_, _, otype, objid, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _) =>
       log.info(s"Received $msg.")
       try {
-        db.withTransaction {
-          Query(TabZhrMds002s).filter(_.otype === otype).filter(_.objid === objid).delete
-          TabZhrMds002s.insert(TabZhrMds002.unapply(msg).get)
+        db.withDynTransaction {
+          val tabZhrMds002s = TableQuery[TabZhrMds002s]
+          tabZhrMds002s.filter(_.otype === otype).filter(_.objid === objid).delete
+          tabZhrMds002s.insert(msg)
         }
         log.info(s"Saved $msg.")
       } catch {
@@ -56,9 +42,10 @@ class DbConnector extends Actor with ActorLogging {
     case msg @ TabZhrMds003(_, _, otype, objid, relat, sclas, sobid, _, _, _, _, _, _, _, _) =>
       log.info(s"Received $msg.")
       try {
-        db.withTransaction {
-          Query(TabZhrMds003s).filter(_.otype === otype).filter(_.objid === objid).filter(_.relat === relat).filter(_.sclas === sclas).filter(_.sobid === sobid).delete
-          TabZhrMds003s.insert(TabZhrMds003.unapply(msg).get)
+        db.withDynTransaction {
+          val tabZhrMds003s = TableQuery[TabZhrMds003s]
+          tabZhrMds003s.filter(_.otype === otype).filter(_.objid === objid).filter(_.relat === relat).filter(_.sclas === sclas).filter(_.sobid === sobid).delete
+          tabZhrMds003s.insert(msg)
         }
         log.info(s"Saved $msg.")
       } catch {
@@ -68,8 +55,9 @@ class DbConnector extends Actor with ActorLogging {
     case msg @ SyncLog(lastDate, processedAt) =>
       log.info(s"Received $msg.")
       try {
-        db.withTransaction {
-          SyncLogs.insert(lastDate, processedAt)
+        db.withDynTransaction {
+          val syncLogs = TableQuery[SyncLogs]
+          syncLogs.insert(msg)
         }
         log.info(s"Saved $msg.")
       } catch {
